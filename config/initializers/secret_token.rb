dc504d49dd1705d7df46af91030cf82b9ded3f1d
TRS710 <navatar.research.work@gmail.com>
2013-11-29 20:53:09 +0530

    Initailised Repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Demo3::Application.config.secret_key_base = '59695c0cc41c56a03b3776c4c32959bc53844557e034691b72bcedaa1176a467579867c660f28bee4a08d1dcf45ee5a0d8d19a9e5313bf2539c486f972683ccc'
